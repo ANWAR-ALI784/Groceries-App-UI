44015e645024a09e15c2531444a9a1676cb8f94d
ANWAR-ALI784 <anwaralimarwat39@gmail.com>
2025-04-23 14:42:57 +0500

    in this we load data from json file and how we can acess the json file intothrough map

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -21,6 +21,7 @@ class _HomepageState extends State<Homepage> {
     loadData();
   }
   loadData()async{
+    await Future.delayed(Duration(seconds: 2));
     final catalogJson=await rootBundle.loadString("assets/files/catalog.json");
     final decodeData=jsonDecode(catalogJson);
     var productData=decodeData["products"];
@@ -52,10 +53,10 @@ class _HomepageState extends State<Homepage> {
 
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(itemCount: CatalogModel.items.length, //CatalogModel.items
+        child: (CatalogModel.items !=null && CatalogModel.items.isNotEmpty)? ListView.builder(itemCount: CatalogModel.items.length, //CatalogModel.items
         itemBuilder: (context,index){
           return ItemWidget(item:CatalogModel.items[index]); //CatalogModel.items
-        },),
+        },):Center(child:CircularProgressIndicator() ,),
       ),
       drawer: const MyDrawer(),
     );
